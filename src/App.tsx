c8d6ab77bc5302afce37b8f8e93fa5b956c19012
tigercxx <76241201+tigercxx@users.noreply.github.com>
2023-03-18 01:57:35 +0800

    um input is done, but still need write the get req

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,81 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { useRef } from "react";
 
 function App() {
+    const redditRef = useRef<HTMLInputElement>(null);
+    const redditOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(redditRef.current?.value);
+    };
+
+    const sentenceRef = useRef<HTMLInputElement>(null);
+    const sentenceOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(sentenceRef.current?.value);
+    };
+
+    // still can do an input for file
+
     return (
-        <div className="">
+        <div className="bg-[#fefae0">
             <Navbar></Navbar>
-            <h1 className="text-3xl font-bold underline text-red-600">Hi</h1>
-            <h2>This is our app</h2>
-            <label htmlFor="subreddit">Enter the subreddit</label>
-            <br />
-            <input id="subreddit" name="subreddit"></input>
-            <p>Hi</p>
+            <div className="mb-6 p-10">
+                <h1 className="text-3xl font-bold text-[#d4a373] text-center">
+                    Next Generation Brand Research
+                </h1>
+                <h2 className="text-xl font-bold text-[#d4a373] text-center">
+                    Test it out!
+                </h2>
+                {/* Reddit input */}
+                <div className="h-10"></div>
+                <label
+                    htmlFor="subreddit"
+                    className="block mb-2 text-xl font-medium text-[#d4a373] dark:text-[#d4a373"
+                >
+                    Enter the subreddit you are interested in
+                </label>
+                <div className="grid grid-cols-4">
+                    <input
+                        id="subreddit"
+                        ref={redditRef}
+                        name="subreddit"
+                        className="col-span-2 block w-full p-4 text-[#d4a373] border border-[#d4a373] rounded-lg bg-[#fefae0] sm:text-md focus:ring-[#d4a373] focus:border-[#d4a373]"
+                    ></input>
+                    <div className="col-span-1 flex justify-center items-center">
+                        <button
+                            onClick={redditOnClick}
+                            className="bg-[#faedcd] h-1/2 w-1/3 rounded-lg text-[#d4a373] border border-[#d4a373] hover:bg-[#eaddbd] min-w-fit	"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                    <div className="col-span-1 "></div>
+                </div>
+                {/* Sentence input */}
+                <div className="h-10"></div>
+                <label
+                    htmlFor="subreddit"
+                    className="block mb-2 text-xl font-medium text-[#d4a373] dark:text-[#d4a373"
+                >
+                    Enter a sentence to test out our ABSA model
+                </label>
+                <div className="grid grid-cols-4">
+                    <input
+                        id="subreddit"
+                        ref={sentenceRef}
+                        name="subreddit"
+                        className="col-span-2 block w-full p-4 text-[#d4a373] border border-[#d4a373] rounded-lg bg-[#fefae0] sm:text-md focus:ring-[#d4a373] focus:border-[#d4a373]"
+                    ></input>
+                    <div className="col-span-1 flex justify-center items-center">
+                        <button
+                            onClick={sentenceOnClick}
+                            className="bg-[#faedcd] h-1/2 w-1/3 rounded-lg text-[#d4a373] border border-[#d4a373] hover:bg-[#eaddbd] min-w-fit	"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                    <div className="col-span-1 "></div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -4,11 +4,11 @@ export default function Navbar() {
     const [navbarOpen, setNavbarOpen] = React.useState(false);
     return (
         <>
-            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-amber-500 mb-3">
+            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-[#faedcd] mb-3">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
                         <a
-                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                            className="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-[#d4a373]"
                             href="#pablo"
                         >
                             ClosedAI
@@ -19,7 +19,7 @@ export default function Navbar() {
                             className="object-contain h-24 w-48 inline-block leading-relaxed"
                         ></img>
                         <button
-                            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            className="text-[#d4a373] cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                             type="button"
                             onClick={() => setNavbarOpen(!navbarOpen)}
                         >
@@ -36,16 +36,16 @@ export default function Navbar() {
                         <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
                             <li className="nav-item">
                                 <a
-                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-[#d4a373] hover:opacity-75"
                                     href="#pablo"
                                 >
-                                    <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
+                                    <i className="fab fa-facebook-square text-lg leading-lg text-[#d4a373] opacity-75"></i>
                                     <span className="ml-2">Share</span>
                                 </a>
                             </li>
                             <li className="nav-item">
                                 <a
-                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-[#d4a373] hover:opacity-75"
                                     href="#pablo"
                                 >
                                     <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
@@ -54,10 +54,10 @@ export default function Navbar() {
                             </li>
                             <li className="nav-item">
                                 <a
-                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-[#d4a373] hover:opacity-75"
                                     href="#pablo"
                                 >
-                                    <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
+                                    <i className="fab fa-pinterest text-lg leading-lg text-[#d4a373] opacity-75"></i>
                                     <span className="ml-2">Pin</span>
                                 </a>
                             </li>
