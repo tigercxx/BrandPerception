01b886d74076c276cddf6cd313fa9f618a0ff40a
Rui Yan <angruiyan@gmail.com>
2023-03-18 01:28:21 +0800

    Updated navbar and added a linechart test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React from 'react';
+import { LineChart } from './components/charts/LineChart';
+import Navbar from './components/Navbar';
 
 function App() {
-    return (
-        <div className="">
-            <Navbar></Navbar>
-            <h1 className="text-3xl font-bold underline text-red-600">Hi</h1>
-            <h2>This is our app</h2>
-            <label htmlFor="subreddit">Enter the subreddit</label>
-            <br />
-            <input id="subreddit" name="subreddit"></input>
-            <p>Hi</p>
-        </div>
-    );
+	return (
+		<div className="">
+			<Navbar></Navbar>
+			<h1 className="text-3xl font-bold underline text-red-600">Hi</h1>
+			<h2>This is our app</h2>
+			<label htmlFor="subreddit">Enter the subreddit</label>
+			<br />
+			<input
+				id="subreddit"
+				name="subreddit"
+			></input>
+			<p>Hi</p>
+			{/* <LineChart /> */}
+		</div>
+	);
 }
 
 export default App;
