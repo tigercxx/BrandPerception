7e96558ec329808531859e3deee58aeb2667fbfe
Rui Yan <angruiyan@gmail.com>
2023-03-18 15:16:00 +0800

    Added text upload button which can communicate with the backend

--- src/components/TextUploadForm.tsx
+++ src/components/TextUploadForm.tsx
@@ -3,67 +3,43 @@ import { useState } from 'react';
 export const BACKEND_DOMAIN = 'http://localhost:4242/';
 
 const TextUploadForm = () => {
-	const [file, setFile] = useState<File | null>(null);
-
-	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files && event.target.files.length > 0) {
-			setFile(event.target.files[0]);
-		}
-	};
-
-	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+	const showFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
 		event.preventDefault();
 
-		if (!file) {
-			alert('Please select a file');
-			return;
+		if (event.target.files && event.target.files.length > 0) {
+			console.log(event.target.files[0]);
+
+			const reader = new FileReader();
+			reader.onload = async (event) => {
+				const text = event.target!.result;
+
+				// split by the new lines for both CRLF and LF, and filter only with content
+				const data = {
+					body: text?.toString().split(/\r?\n/).filter(Boolean),
+				};
+
+				const response = await fetch(BACKEND_DOMAIN + 'predict_file', {
+					method: 'POST',
+					headers: { 'content-type': 'application/json' },
+					body: JSON.stringify(data),
+				});
+
+				if (!response.ok) {
+					throw new Error('Failed to send');
+				}
+			};
+			reader.readAsText(event.target!.files[0]);
 		}
-
-		const formData = new FormData();
-		formData.append('avatar', file);
-		console.log(formData);
-		console.log(file);
-
-		const response = await fetch(BACKEND_DOMAIN + 'predict_text', {
-			method: 'POST',
-			headers: { 'content-type': 'application/json' },
-			body: JSON.stringify(file),
-		});
-
-		if (!response.ok) {
-			throw new Error('Failed to send');
-		}
-
-		const result = await response.json();
-		console.log(result);
 	};
 
 	return (
 		<div>
-			<form
-				onSubmit={handleSubmit}
-				method="post"
-				encType="multipart/form-data"
-			>
-				<label>
-					Select a file:
-					<input
-						type="file"
-						name="avatar"
-						onChange={handleFileChange}
-					/>
-				</label>
-				<button type="submit">Submit</button>
-			</form>
 			<div>
-				<form
-					action="/predict_text"
-					method="post"
-					encType="multipart/form-data"
-				>
+				<form encType="multipart/form-data">
 					<input
 						type="file"
-						name="avatar"
+						name="text_to_upload"
+						onChange={(e) => showFile(e)}
 					/>
 				</form>
 			</div>
