964c4f84d5a8b8595659cc28b2218186ae93ef78
Rui Yan <angruiyan@gmail.com>
2023-03-20 10:22:08 +0800

    Added linechart

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BarChart } from './charts/BarChart';
+import { LineChart } from './charts/LineChart';
 
 function Dashboard() {
 	return (
 		<div>
 			<BarChart />
+			<LineChart />
 		</div>
 	);
 }
