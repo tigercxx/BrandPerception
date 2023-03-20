762b27f657e5087e1251812ecae13f02155cd450
tigercxx <76241201+tigercxx@users.noreply.github.com>
2023-03-21 02:27:08 +0800

    f

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-// const BACKEND_DOMAIN = 'http://localhost:4242/';
-const BACKEND_DOMAIN = "http://54.169.56.122:4242/";
+const BACKEND_DOMAIN = "http://localhost:4242/";
+// const BACKEND_DOMAIN = "http://54.169.56.122:4242/";
 
 export { BACKEND_DOMAIN };
